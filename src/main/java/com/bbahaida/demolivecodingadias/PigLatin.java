6a6dc9c0376fba6079f980e592ca35922f710d8d
Brahim Bahaida <bbahieda@gmail.com>
2019-12-29 19:37:14 +0000

    pig latin only regex solution

--- src/main/java/com/bbahaida/demolivecodingadias/PigLatin.java
+++ src/main/java/com/bbahaida/demolivecodingadias/PigLatin.java
@@ -1,23 +1,10 @@
 package com.bbahaida.demolivecodingadias;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public class PigLatin {
-
-    private static final String SUFFIX = "ay";
-    private static final String SPACE_REGEX = "\\s";
-    private static final String SPECIAL_CHARACTERS_REGEX = "[\\^$.|?*!+()]";
+    private static final String PIG_REGEX = "(\\w)(\\w*)";
+    private static final String REPLACE_REGEX = "$2$1ay";
 
     public static String pigIt(String str) {
-        return Arrays.stream(str.split(SPACE_REGEX))
-                .map(word ->  new StringBuffer(word.substring(1)).append(word.charAt(0)))
-                .map(word -> {
-                    if (!word.toString().matches(SPECIAL_CHARACTERS_REGEX)) {
-                        return word.append(SUFFIX);
-                    }
-                    return word;
-                })
-                .collect(Collectors.joining(" "));
+        return str.replaceAll(PIG_REGEX, REPLACE_REGEX);
     }
 }
