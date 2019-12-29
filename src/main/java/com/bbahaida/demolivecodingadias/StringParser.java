661baa4584922fe2b10b223f1ea1f4cd20394c82
Brahim Bahaida <bbahieda@gmail.com>
2019-12-29 15:42:47 +0000

    parse string answer

--- src/main/java/com/bbahaida/demolivecodingadias/StringParser.java
+++ src/main/java/com/bbahaida/demolivecodingadias/StringParser.java
@@ -1,8 +1,15 @@
 package com.bbahaida.demolivecodingadias;
 
+import java.util.Arrays;
+
 public class StringParser {
 
+    private static final String SEPARATORS = "(,|\n|;|\t)+";
+    private static final String COMMA = ",";
+
     public static int sum(String str) {
-        return 0;
+        return Arrays.stream(str.replaceAll(SEPARATORS, COMMA).split(COMMA))
+                .mapToInt(Integer::valueOf)
+                .sum();
     }
 }
