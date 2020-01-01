ff2a90bb12a288464f70a6114d7fb2d118519201
Brahim Bahaida <bbahieda@gmail.com>
2020-01-01 15:38:13 +0000

    Order better solution

--- src/main/java/com/bbahaida/demolivecodingadias/Order.java
+++ src/main/java/com/bbahaida/demolivecodingadias/Order.java
@@ -1,8 +1,7 @@
 package com.bbahaida.demolivecodingadias;
 
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class Order {
@@ -10,17 +9,7 @@ public class Order {
 
         if (words.length() == 0) return "";
         return Arrays.stream(words.split("\\s"))
-                .sorted(Order::orderByNumericPattern)
+                .sorted(Comparator.comparing(s -> Integer.valueOf(s.replaceAll("\\D", ""))))
                 .collect(Collectors.joining(" "));
-    }
-
-    private static int orderByNumericPattern(String word1, String word2) {
-        Pattern pattern = Pattern.compile("(\\d+)");
-        Matcher matcher1 = pattern.matcher(word1);
-        Matcher matcher2 = pattern.matcher(word2);
-        if (matcher1.find() && matcher2.find()) {
-            return Integer.valueOf(matcher1.group(1)).compareTo(Integer.valueOf(matcher2.group(1)));
-        }
-        throw new RuntimeException("No matching exception");
     }
 }
